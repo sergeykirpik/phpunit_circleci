981e98ace7ab6d7d0632626f2e5112863fcba2cb
Sergey Kirpik <sergeykirpik@gmail.com>
2020-06-25 07:38:53 +0300

    Full Mock Example: the Sequel

--- tests/AppBundle/Service/EnclosureBuilderServiceTest.php
+++ tests/AppBundle/Service/EnclosureBuilderServiceTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\AppBundle\Service;
 
+use AppBundle\Entity\Dinosaur;
+use AppBundle\Entity\Enclosure;
 use AppBundle\Factory\DinosaurFactory;
 use AppBundle\Service\EnclosureBuilderService;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,15 +14,25 @@ class EnclosureBuilderServiceTest extends TestCase
     public function testItBuildsAndPersistsEnclosure()
     {
         $em = $this->createMock(EntityManagerInterface::class);
+        $em->expects($this->once())
+            ->method('persist')
+            ->with($this->isInstanceOf(Enclosure::class));
+
+        $em->expects($this->atLeastOnce())
+            ->method('flush');
+
         $dinoFactory = $this->createMock(DinosaurFactory::class);
         $dinoFactory->expects($this->exactly(2))
             ->method('growFromSpecification')
+            ->willReturn(new Dinosaur())
             ->with($this->isType('string'));
 
         $builder = new EnclosureBuilderService($em, $dinoFactory);
         $enclosure = $builder->buildEnclosure(1, 2);
 
         $this->assertCount(1, $enclosure->getSecurities());
-        //$this->assertCount(2, $enclosure->getDinosaurs());
+        $this->assertCount(2, $enclosure->getDinosaurs());
+
+        dump($enclosure->getDinosaurs()->toArray());
     }
 }
